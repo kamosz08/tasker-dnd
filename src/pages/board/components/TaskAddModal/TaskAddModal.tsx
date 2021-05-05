c2fd4a31a86c4d95bf659c7231866418ff8a1def
Kamil Szczerbiński <k.szczerbinski@neurosys.com>
2021-05-05 19:37:09 +0200

    Add task modal

--- src/pages/board/components/TaskAddModal/TaskAddModal.tsx
+++ src/pages/board/components/TaskAddModal/TaskAddModal.tsx
@@ -1,12 +1,24 @@
-import { Button, TextField } from "@material-ui/core";
-import React from "react";
+import { Box, Button, TextField, Tooltip } from "@material-ui/core";
+import React, { useState } from "react";
 import Modal from "react-modal";
 import styles from "./styles.module.css";
+import LabelOutlinedIcon from "@material-ui/icons/LabelOutlined";
+import LinkOutlinedIcon from "@material-ui/icons/LinkOutlined";
+import { Field, Form, Formik, FormikHelpers, useFormikContext } from "formik";
+import { useHistory } from "react-router";
+import { FormikInput } from "../../../../shared/FormikInput/FormikInput";
 
 type Props = {
   show: boolean;
   onClose: () => void;
 };
+
+type FormValues = {
+  title: string;
+  description: string;
+};
+
+type Errors = Partial<FormValues>;
 
 Modal.setAppElement("#root");
 
@@ -14,8 +26,8 @@ const customStyles = {
   content: {
     width: 960,
     height: "min-content",
-    minHeight: 400,
-    maxHeight: 700,
+    minHeight: 300,
+    maxHeight: 600,
     margin: "auto",
     top: 0,
     left: 0,
@@ -24,48 +36,35 @@ const customStyles = {
   },
 };
 
-export const TaskAddModal: React.FC<Props> = ({ show, onClose }) => {
+const AddTaskForm: React.FC = () => {
+  const { isSubmitting } = useFormikContext<FormValues>();
+
   return (
-    <Modal isOpen={show} onRequestClose={onClose} style={customStyles}>
-      <div className={styles.wrapper}>
-        <section className={styles.main}>
-          <div className={styles["flex-row"]}>
-            <div className={styles["icon-button"]}>X</div>
-            <div className={styles["icon-button"]}>X</div>
-            <div className={styles["icon-button"]}>X</div>
-            <div className={styles["icon-button"]}>X</div>
-          </div>
-          <div className={styles["flex-row"]}>
-            <span className={styles.small}>
-              <span className={styles["name"]}>BRD-3212</span>
-            </span>
-            <span className={styles.small}>
-              Created by <span className={styles["name"]}>John Kaszubikx</span>{" "}
-              a week ago
-            </span>
-            <span className={styles.small}>
-              Updated by <span className={styles["name"]}>Mayouko</span> 2 hours
-              ago
-            </span>
-          </div>
-          <p className={styles.title}>Task title here</p>
-          <p className="description">
-            Task description here asdasd asdasd asd asdasd as dasdas dasd
-          </p>
-          <TextField
-            name="addComment"
+    <Form>
+      <Box display="flex" height="100%">
+        <Box flex={3} paddingRight="16px">
+          <Box display="flex" marginBottom="16px">
+            <Tooltip title="Add tag" placement="top">
+              <div className={styles["icon-button"]}>
+                <LabelOutlinedIcon />
+              </div>
+            </Tooltip>
+            <Tooltip title="Link another task" placement="top">
+              <div className={styles["icon-button"]}>
+                <LinkOutlinedIcon />
+              </div>
+            </Tooltip>
+          </Box>
+          <Field name="title" placeholder="title" component={FormikInput} />
+          <Field
+            name="description"
+            placeholder="Description"
             multiline
-            placeholder="Write a comment"
             rows={4}
-            className={styles["add-comment"]}
+            component={FormikInput}
           />
-          <Button variant="outlined" color="primary">
-            Add comment
-          </Button>
-          <p className="tabs">Tabs - style me</p>
-          <div className="comment-list">Comment list </div>
-        </section>
-        <section className={styles["right-sidebar"]}>
+        </Box>
+        <Box flex={1}>
           <div className={styles["info-card"]}>
             {[1, 2, 3, 4, 5].map((item) => (
               <React.Fragment key={item}>
@@ -74,8 +73,71 @@ export const TaskAddModal: React.FC<Props> = ({ show, onClose }) => {
               </React.Fragment>
             ))}
           </div>
-        </section>
-      </div>
+        </Box>
+      </Box>
+      <Box
+        width="100%"
+        display="flex"
+        justifyContent="flex-end"
+        marginTop="32px"
+      >
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          disableElevation
+          disabled={isSubmitting}
+        >
+          Save
+        </Button>
+      </Box>
+    </Form>
+  );
+};
+
+export const TaskAddModal: React.FC<Props> = ({ show, onClose }) => {
+  const [error, setError] = useState("");
+  const { push } = useHistory();
+  const validate = (values: FormValues) => {
+    const errors: Errors = {};
+    if (!values.title) {
+      errors.title = "Required";
+    } else if (values.title.length < 4) {
+      errors.title = "Minimum title length is 4";
+    } else if (values.title.length > 80) {
+      errors.title = "Maximum title length is 80";
+    }
+
+    return errors;
+  };
+
+  const onSubmit = async (
+    values: FormValues,
+    { setSubmitting }: FormikHelpers<FormValues>
+  ) => {
+    try {
+      setError("");
+      // await logIn(values.email, values.password);
+      push("/");
+    } catch (error) {
+      if (typeof error.message === "string") {
+        setError(error.message);
+      } else {
+        setError("Failed to create a task");
+      }
+    }
+    setSubmitting(false);
+  };
+
+  return (
+    <Modal isOpen={show} onRequestClose={onClose} style={customStyles}>
+      <Formik
+        initialValues={{ title: "", description: "" }}
+        validate={validate}
+        onSubmit={onSubmit}
+      >
+        <AddTaskForm />
+      </Formik>
     </Modal>
   );
 };
